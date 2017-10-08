041d4f42cded8fca2f8db78e7ae1727d1c425a2d
Etan Imanol Castro Aldrete <A01209675@itesm.mx>
2017-10-08 18:34:24 -0500

    All Field like Memory_address renamed to MemoryAddress

--- api/models.py
+++ api/models.py
@@ -1,37 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Building(models.Model):
     Name = models.CharField(max_length=255)
-    Gpslatitude = models.CharField(max_length=255)
-    Gpslongitude = models.CharField(max_length=255)
+    GpsLatitude = models.CharField(max_length=255)
+    GpsLongitude = models.CharField(max_length=255)
 
 class Group_Nodes(models.Model):
     Name = models.CharField(max_length=255)
-    Building = models.ForeignKey(Building)
+    Building = models.ForeignKey(Building, related_name="Nodes")
     def __str__(self):
         return self.Name
 
 class Node(models.Model): #One node equivalente one rpi with n accelerometers
-    User_id = models.ForeignKey(User) #Owner
+    UserId = models.ForeignKey(User) #Owner
     Shared = models.ManyToManyField(User,related_name="Shared_with") #Shared with ..
     Synced = models.BooleanField(default=False)
     Groups_a = models.ManyToManyField(Group_Nodes, related_name="Buildings")
-    Ping_time = models.IntegerField()
+    PingTime = models.IntegerField()
     def __str__(self):
         return self.Name
 
 class Accelerometer(models.Model):
-    Memory_Address = models.CharField(max_length=255)
+    MemoryAddress = models.CharField(max_length=255)
     Status = models.BooleanField() #False if because it is dead
     
 class Acceleration_components(models.Model):
-    Accelerometer_id = models.ForeignKey(Accelerometer, related_name="data") #(x,y,z) vs time
-    Server_time = models.DateField(auto_now_add=True)
+    AccelerometerId = models.ForeignKey(Accelerometer, related_name="data") #(x,y,z) vs time
+    ServerTime = models.DateField(auto_now_add=True)
     Accelerometer_time = models.DateField()
-    X_value = models.FloatField()
-    Y_value = models.FloatField()
-    Z_value = models.FloatField()
+    XValue = models.FloatField()
+    YValue = models.FloatField()
+    ZValue = models.FloatField()
     def __str__(self):
         return str(X_value)+"-"+str(Y_value)+"-"+str(Z_value) #Returning as X-Y-Z
