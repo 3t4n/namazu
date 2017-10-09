33c0923de894723eea48e8009d12d2880dfec16a
Etan Imanol Castro Aldrete <A01209675@itesm.mx>
2017-10-08 20:22:37 -0500

    Registered all models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+import inspect
+import api.models as models
+from django.db.models.base import ModelBase
 
-# Register your models here.
+#Register ALL models
+for model_name in dir(models):
+    model = getattr(models, model_name)
+    if isinstance(model, ModelBase) and  not(model._meta.abstract):
+        admin.site.register(model)
