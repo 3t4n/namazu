b5cb8689d22900ba5df965617cab2def4b8cfa2f
Etan Imanol Castro Aldrete <A01209675@itesm.mx>
2017-10-08 20:26:58 -0500

    Added condition model_name!=User

--- api/admin.py
+++ api/admin.py
@@ -6,5 +6,5 @@ from django.db.models.base import ModelBase
 #Register ALL models
 for model_name in dir(models):
     model = getattr(models, model_name)
-    if isinstance(model, ModelBase) and  not(model._meta.abstract):
+    if isinstance(model, ModelBase) and  not(model._meta.abstract) and model_name!="User":
         admin.site.register(model)
